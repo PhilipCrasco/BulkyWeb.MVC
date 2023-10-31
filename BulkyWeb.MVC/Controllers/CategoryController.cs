0de818a65bae0367e2e2ff748175b2c40f6c347d
philip crasco <philiplorenzcrasco@gmail.com>
2023-10-31 16:46:59 +0800

    Add post crud

---
+++ BulkyWeb.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using BulkyWeb.MVC.Context;
+using BulkyWeb.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly DataContext _context;
+        public CategoryController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+
+            var categories = new Category
+            {
+                Name = category.Name,
+                Display_Order = category.Display_Order
+
+            };
+
+            _context.Add(categories);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ BulkyWeb.MVC/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.MVC.Models
+{
+    public class Category : BaseEntity
+    {
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        public int Display_Order { get; set; }
+
+
+    }
+}
